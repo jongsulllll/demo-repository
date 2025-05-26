9aa724bc9014b29410c7bb30f75ea8d509efbc6c
Rakyung <77048462+Rakyung@users.noreply.github.com>
2025-05-26 17:11:21 +0900

    Add files via upload

---
+++ final/0526위험인식하고포즈감지까지/media.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "4abc0892-c103-4d31-99c1-511400d66010",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
